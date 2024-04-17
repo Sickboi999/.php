d5553af6874be2e1a8bd4920cf9d9da3fb9f9f6e
Sickboi999 <161617202+Sickboi999@users.noreply.github.com>
2024-04-17 23:00:59 +0800

    Create intro.php

---
+++ week03/intro.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>Page of HandsomeBoi</title>
+    </head>
+    <body bgcolor = D7CCC8>
+        
+        <b><i><font size = +6 color = FB8C00>陳咨宇</font></i></b><br/>
+        <img src = "chewmi.jpg" height = 200><br/>
+        <b><font size = +2>學歷</font></b><br/>
+        <HR>
+        <ul>
+            <li>建國國小附設幼兒園</li>
+            <li>建國國小</li>
+            <li>鶯歌國中</li>
+            <li>板橋高中</li>
+            <li>高雄大學</li>
+        </ul>
+        <b><font size = +2>介紹</font></b><br/>
+        <HR>
+        台灣人，男性，牡羊座，現就讀高雄大學<br/>
+        <b><font size = +2>聯絡</font></b><br/>
+        <HR>
+        <ul type = "disc">
+            <li><font color = 29B6F6> 藥助理</font> CELL : 0966-785-711 <a href = "https://www.facebook.com/T1.Anthon">Facebook</a></li>
+            <li><font color = 7E57C2>撞助理</font> CELL : 0979-551-891 <a href = "https://findtheinvisiblecow.com/">Cow</a></li>
+        </ul>
+    </body>
+</html>
